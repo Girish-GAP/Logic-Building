6aef7662fdcb0966ebf3bf5c45fac28093b9ad48
Girish <82331285+Girish-GAP@users.noreply.github.com>
2021-10-30 11:33:05 +0530

    Solution for Problem 4

---
+++ C/Problems on Numbers/Problem_004.c
@@ -0,0 +1,61 @@
+//////////////////////////////////////////////////////////////////////////////////
+//
+// Problem statement : Write a program which accept number from user and print 
+//                     even factors of that number (excluding itself).
+//
+// Test cases :
+// Input  : 36 
+// Output : 2   4   6   12  18
+//
+// Author : Girish Pawar
+// Date : 02-10-2021 
+// 
+/////////////////////////////////////////////////////////////////////////////////////
+
+// Header file inclusion
+#include<stdio.h>
+
+// Fucntion declaration
+void DisplayEvenFactor(int);
+
+
+// Entry point function 
+int main()
+{
+    int iValue = 0;
+
+    printf("Enter a number : ");
+    scanf("%d", &iValue);
+
+    DisplayEvenFactor(iValue);
+
+    return 0;
+}
+
+
+// Function defintion
+void DisplayEvenFactor(int iNo)
+{
+    int iCnt = 0;
+
+    if (iNo < 0)    // Input updator 
+    {
+        iNo = -iNo;
+    }
+
+    if (iNo == 0)   // Filter
+    {
+        printf("Your Enterd number is 0 !");
+    } 
+
+    for(iCnt = 1; iCnt <= iNo/2; iCnt++)      // Factors are always upto half of number excluding itself 
+    {
+        if( ((iNo % iCnt) == 0) && ((iCnt % 2) == 0) )
+        {
+            printf("%d\t", iCnt);
+        }
+    }
+}
+
+// Time Complexity : O(n/2)
+// Where n = iNo (input number)
