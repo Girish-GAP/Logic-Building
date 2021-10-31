57348efeacfdf175c6ebca326959b5d4ce1636a1
Girish <82331285+Girish-GAP@users.noreply.github.com>
2021-10-31 12:32:00 +0530

    Solution for Problem 20

---
+++ C/Problems on Pattern Printing/Problem_020.c
@@ -0,0 +1,66 @@
+//////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Problem statement : Accept number of rows and columns from user and display below pattern.
+//
+// Input  : iRow = 4     iCol = 4
+// Output : 1   2   3   4
+//          1   *   *   4
+//          1   *   *   4
+//          1   2   3   4
+//
+// Author : Girish Ashok Pawar
+// Date : 23-09-2021
+//
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+// Header file inclusion
+#include <stdio.h>
+
+// Function declaration
+void Pattern(int, int);
+
+// Entry point function
+int main()
+{
+    int iValue1 = 0; // Local variables
+    int iValue2 = 0;
+
+    printf("Enter number of rows : ");
+    scanf("%d", &iValue1);
+
+    printf("Enter numbers of columns : ");
+    scanf("%d", &iValue2);
+
+    Pattern(iValue1, iValue2); // Function call
+
+    return 0;
+}
+
+// Function defination
+void Pattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    for (i = 1; i <= iRow; i++) // For Rows
+    {
+        for (j = 1; j <= iCol; j++) // For Columns
+        {
+            if (j == 1 || j == iCol)
+            {
+                printf("%d\t", j);
+            }
+            else if (i == 1 || i == iRow)
+            {
+                printf("%d\t", j);
+            }
+            else
+            {
+                printf("*\t");
+            }
+        }
+        printf("\n");
+    }
+}
+
+// Time Complexity : O(n^2)
+// Where, n = Number of rows & columns
