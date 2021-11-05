6fdfed01d3ad1393798e64b807ae4cfa5e55683a
Girish <82331285+Girish-GAP@users.noreply.github.com>
2021-11-05 10:59:18 +0530

    Solution for Problem 16

---
+++ C/Problems on Array/Problem_016.c
@@ -0,0 +1,73 @@
+///////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Problem statement : Accept N numbers from user and return largest number.
+//
+// Input  : N : 6
+//          Elements : 85   66  3   76  93  88
+// Output : 93
+//
+// Author : Girish Ashok Pawar
+// Date : 27/09/2021
+//
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+// Header file inclusion
+#include <stdio.h>
+#include <stdlib.h>
+
+// Function declaration
+int Maximum(int Arr[], int Length);
+
+// Entry point function
+int main()
+{
+    int iSize = 0, iRet = 0, iCnt = 0; // Local variables
+    int *p = NULL;
+
+    printf("Enter number of elements : ");
+    scanf("%d", &iSize);
+
+    p = (int *)malloc(iSize * sizeof(int)); // Dynamic memory allocation
+
+    if (p == NULL) // If memory is not allocated
+    {
+        printf("Unable to allocate memory : ");
+        return -1;
+    }
+
+    printf("\nEnter %d elements one by one : ", iSize);
+
+    for (iCnt = 0; iCnt < iSize; iCnt++) // Accept array elements
+    {
+        scanf("%d", &p[iCnt]);
+    }
+
+    iRet = Maximum(p, iSize); // Function call
+
+    printf("Largest number is : %d", iRet);
+
+    free(p); // free memory
+
+    return 0;
+}
+
+// Function defination
+int Maximum(int Arr[], int Length)
+{
+    // Logic
+
+    int i = 0, iMax = Arr[0];
+
+    for (i = 0; i < Length; i++)
+    {
+        if (Arr[i] > iMax)
+        {
+            iMax = Arr[i];
+        }
+    }
+
+    return iMax;
+}
+
+// Time complexity : O(n)
+// Where n = size of array
